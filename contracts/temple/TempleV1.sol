e10d07694da3bd6cdf304055d9331902843d5836
BenTokenFinance <admin@bentoken.finance>
2021-07-15 13:48:42 -0700

    232323

--- contracts/temple/TempleV1.sol
+++ contracts/temple/TempleV1.sol
@@ -3,15 +3,19 @@
 pragma solidity 0.6.12;
 
 import '../libs/SafeBEP20.sol';
+import '../libs/BEP20.sol';
 //import 'https://github.com/BenTokenFinance/bentoken/blob/main/contracts/libs/SafeBEP20.sol';
+//import 'https://github.com/BenTokenFinance/bentoken/blob/main/contracts/libs/BEP20.sol';
 
 contract Temple is Ownable {
-    SafeBEP20 public gben;
+    using SafeBEP20 for BEP20;
+
+    BEP20 public gben;
 
     event Worship(address indexed user, uint256 amount, string prayer);
 
     constructor(
-        SafeBEP20 _gben
+        BEP20 _gben
     ) public {
         gben = _gben;
     }
